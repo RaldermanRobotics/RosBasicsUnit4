81c95107b94b865139e77d5267dbe3c98e94e7ab
raldermanrobotics <raldermanrobotics@gmail.com>
2023-10-12 21:31:23 +0000

    reached the end of part 7. Continuing to learn and enjoying it all

--- move_solution/src/move_solution.cpp
+++ move_solution/src/move_solution.cpp
@@ -2,6 +2,7 @@
 #include <ardrone_as/ArdroneAction.h> // Note: "Action" is appended
 #include <geometry_msgs/Twist.h>
 #include <ros/ros.h>
+#include <std_msgs/Empty.h>
 
 int nImage = 0;
 
@@ -9,7 +10,7 @@ void doneCb(const actionlib::SimpleClientGoalState &state,
             const ardrone_as::ArdroneResultConstPtr &result) {
   ROS_INFO("[State Result]: %s", state.toString().c_str());
   ROS_INFO("The Action has been completed");
-  ros::shutdown();
+  // ros::shutdown();
 }
 
 // Called once when the goal becomes active
@@ -21,47 +22,60 @@ void feedbackCb(const ardrone_as::ArdroneFeedbackConstPtr &feedback) {
 }
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "move");
-  ros::NodeHandle n;
+  ros::init(argc, argv, "drone_action_client");
+  ros::NodeHandle nh;
+
   actionlib::SimpleActionClient<ardrone_as::ArdroneAction> client(
       "ardrone_action_server", true);
   client.waitForServer();
 
+  ros::Publisher move = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
+  geometry_msgs::Twist move_msg;
+  ros::Publisher takeoff =
+      nh.advertise<std_msgs::Empty>("/drone/takeoff", 1000);
+  std_msgs::Empty takeoff_msg;
+  ros::Publisher land = nh.advertise<std_msgs::Empty>("/drone/land", 1000);
+  std_msgs::Empty land_msg;
+
   ardrone_as::ArdroneGoal goal;
   goal.nseconds = 10;
+
   client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
   // client.waitForResult();
 
-  ros::Rate loop_rate(2);
+  ros::Rate loop_rate(1);
   actionlib::SimpleClientGoalState state_result = client.getState();
   ROS_INFO("[State Result]: %s", state_result.toString().c_str());
 
-  ros::Publisher effector = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
-
-  geometry_msgs::Twist thingout;
-  thingout.linear.x = 0.5;
-  thingout.angular.z = 0.5;
-
-  geometry_msgs::Twist nomove;
-  nomove.linear.x = 0;
-  nomove.angular.z = 0;
+  int i = 0;
+  while (i < 4) {
+    takeoff.publish(takeoff_msg);
+    ROS_INFO("Taking Off Drone...");
+    loop_rate.sleep();
+    i++;
+  }
 
   while (state_result == actionlib::SimpleClientGoalState::ACTIVE ||
          state_result == actionlib::SimpleClientGoalState::PENDING) {
-    ROS_INFO("hopefully moving");
-    effector.publish(thingout);
-    ros::spinOnce();
+    ROS_INFO(
+        "Moving drone around while waiting for the Server to give a result...");
+    move_msg.linear.x = 1;
+    move_msg.angular.z = 1;
+    move.publish(move_msg);
     loop_rate.sleep();
     state_result = client.getState();
     ROS_INFO("[State Result]: %s", state_result.toString().c_str());
-    if (state_result == actionlib::SimpleClientGoalState::SUCCEEDED) {
-      ROS_INFO("Success and we're stopping");
-      effector.publish(nomove);
-      ros::spinOnce();
-      loop_rate.sleep();
-      state_result = client.getState();
-      ROS_INFO("[State Result]: %s", state_result.toString().c_str());
-    }
+  }
+
+  i = 0;
+  while (i < 4) {
+    move_msg.linear.x = 0;
+    move_msg.angular.z = 0;
+    move.publish(move_msg);
+    land.publish(land_msg);
+    ROS_INFO("Landing Drone...");
+    loop_rate.sleep();
+    i++;
   }
 
   return 0;
