dc887422ff1aad51e5d2a50f94bc2c21f05fa76d
raldermanrobotics <raldermanrobotics@gmail.com>
2023-10-15 16:52:30 +0000

    troubleshooting the catkin_make of the practice_ck2 package

--- practice_ck2/src/ros_service.cpp
+++ practice_ck2/src/ros_service.cpp
@@ -1,28 +1,68 @@
 #include "geometry_msgs/Twist.h"
+#include "nav_msgs/Odometry.h"
 #include "ros/ros.h"
 #include "std_srvs/Empty.h"
 
 class Rotate {
 
   ros::NodeHandle node;
-  actionlib::SimpleActionServer<actionlib::TestAction> as_;
-  std::string action_name_;
-  actionlib::TestFeedback information_;
-  actionlib::TestResult result_;
+  // actionlib::SimpleActionServer<actionlib::TestAction> as_;
+  // std::string action_name_;
+  // actionlib::TestFeedback information_;
+  // actionlib::TestResult result_;
 
-  int degrees_;
-  bool success_;
+  // need to make custom of degrees... maybe success...
+  // needed messages
+  int degrees;
+  bool success;
 
-  ros::Rate rate_;
-  ros::Publisher rotate_pub_;
   geometry_msgs::Twist rotate_msg_;
-  ros::Subscriber odom_read_;
-  
 
-      bool
-      simplecallback(std_srvs::Empty::Reuest &req,
-                     std_srvs::Empty::Response &res) {
+  // remember to make custom int "degrees" for the package
+  bool simplecallback(std_srvs::degrees::Request &req,
+                      std_srvs::success::Response &res) {
+    req.degrees = req.degrees * 3.14 / 180;
     ROS_INFO("You have choosen to rotate the robot");
     return true;
   }
+  void odomCb(const nav_msgs::Odometry::ConstPrt &msg) {
+    int orient_z_initial = msg->pose.pose.orientation.z;
+    int angular_z_initial = msg->twist.twist.angular.z;
+  }
+
+  ros::Publisher rotate_pub =
+      node.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+
+  int counter = 0
+
+                if counter < 4 {
+    //  ros::Rate loop_rate(.25); don't think it need to be here... likely to
+    //  remove
+
+    geometry_msgs::Twist angular_accel;
+    angular_accel.angular.z =
+        (req.degrees - orient_z_initial - (angular_z_initial * 4)) / 16;
+    // us angular moment equation at t = 4
+    ros::Time beginTime = ros::Time::now();
+    ros::Duration secondsIWantToSendMessagesFor = ros::Duration(4);
+    ros::Time endTime = beginTime + secondsIWantToSendMessagesFor;
+  }
+
+  while (ros::Time::now() < endTime) {
+    rotate_pub.publish(angular_accel);
+
+    // Time between messages, so you don't blast out an thousands of
+    // messages in your 3 secondperiod
+    ros::Duration(0.1).sleep();
+  }
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "Rotate_robot");
+
+  Rotate your_choice("rotate_robot_ss");
+  ros::spin();
+
+  return 0;
 }
---
+++ testing_service/src/testing.cpp
@@ -0,0 +1,10 @@
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Twist.h>
+
+double roll = 0.0
+double pitch = 0.0
+double yaw = 0.0
+double kP = .5
+
+
